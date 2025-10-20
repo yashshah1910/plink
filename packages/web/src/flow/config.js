37fa20a3a67514900d75fce12ad67d0d9b514468
CommitNinjaX <yashshah192001@gmail.com>
2025-10-20 21:10:42 +0530

    Add testnet account config and env support for Flow

--- packages/web/src/flow/config.js
+++ packages/web/src/flow/config.js
@@ -5,16 +5,19 @@ import { config } from "@onflow/fcl";
 // Configure FCL for emulator development
 config({
   // Points FCL to the Flow Emulator running on your computer
-  "accessNode.api": "http://localhost:8888",
-  
+  "accessNode.api":
+    process.env.NEXT_PUBLIC_FCL_ACCESS_NODE || "http://localhost:8888",
+
   // Local dev wallet for emulator
-  "discovery.wallet": "http://localhost:8701/fcl/authn",
+  "discovery.wallet":
+    process.env.NEXT_PUBLIC_FCL_DISCOVERY_WALLET ||
+    "http://localhost:8701/fcl/authn",
 
   // The name of our dApp to show in wallet pop-ups
   "app.detail.title": "Plink",
   "app.detail.icon": "https://placekitten.com/g/200/200",
 
   // Contract aliases for emulator
-  "0xPlink": "0xf8d6e0586b0a20c7",
-  "0xFungibleToken": "0xee82856bf20e2aa6"
+  "0xPlink": process.env.NEXT_PUBLIC_CONTRACT_PLINK || "0xf8d6e0586b0a20c7",
+  "0xFungibleToken": "0xee82856bf20e2aa6",
 });
